7c7d03e9e5052db9914f65fcf1201ec9ef85e46b
Nicolás Florentín <nicogrunger@hotmail.com>
2022-11-22 03:43:38 -0300

    add scroll up animations

---
+++ scripts/scrollUp-btn.js
@@ -0,0 +1,45 @@
+import anime from "../node_modules/animejs/lib/anime.es.js";
+console.log(anime);
+
+const scrollUpButton = document.querySelector(".scrollUpButton");
+
+// responsive script
+let viewport500_match = window.matchMedia("(max-width: 500px)").matches;
+const button_img = document.querySelector(".scrollUpButton img");
+
+if (viewport500_match) {
+	button_img.style.width = "30px";
+	scrollUpButton.style.right = "20px";
+}
+
+// display inherit on scroll
+let scrollToDisplay;
+if (viewport500_match) {
+	scrollToDisplay = 50;
+} else {
+	scrollToDisplay = 300;
+}
+
+window.onscroll = function () {
+	if (document.body.scrollTop > scrollToDisplay || document.documentElement.scrollTop > scrollToDisplay) {
+		scrollUpButton.style.display = "inherit";
+	} else {
+		scrollUpButton.style.display = "none";
+	}
+};
+
+// onClick animate
+const scrollUpDelay = 400;
+scrollUpButton.addEventListener("click", () => {
+	anime({
+		targets: scrollUpButton,
+		translateY: [
+			{ value: -30, easing: "easeOutElastic(1, .6)" },
+			{ value: 0, easing: "linear", duration: 400, delay: 100 },
+		],
+	});
+	// animation waiting delay
+	setTimeout(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, scrollUpDelay);
+});
