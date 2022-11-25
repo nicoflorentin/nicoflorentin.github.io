79c963f9b3a24ce85b3fa53372cdd966ac1a4810
Nicolás Florentín <nicogrunger@hotmail.com>
2022-11-25 04:24:01 -0300

    add scroll animations

---
+++ scripts/scroll-btn.js
@@ -0,0 +1,34 @@
+const scrollUpButton = document.querySelector('.scrollUpButton');
+const aboutMeButton = document.querySelector('#aboutMe-btn')
+
+// responsive script
+let viewport500_match = window.matchMedia("(max-width: 500px)").matches;
+viewport500_match ? scrollToDisplay = 20 : scrollToDisplay = 300;
+window.onscroll = function () {
+	if (document.body.scrollTop > scrollToDisplay || document.documentElement.scrollTop > scrollToDisplay) {
+		scrollUpButton.style.display = "inherit";
+	} else {
+		scrollUpButton.style.display = "none";
+	}
+};
+
+// onClick animate
+const scrollUpDelay = 400;
+scrollUpButton.addEventListener("click", () => {	
+	anime({
+		targets: scrollUpButton,
+		translateY: [
+			{ value: -30, easing: "easeOutElastic(1, .6)" },
+			{ value: 0, easing: "linear", duration: 400, delay: 100 },
+		],
+	});
+	// animation waiting delay
+	setTimeout(() => {
+		window.scrollTo({ top: 0, behavior: "smooth" });
+	}, scrollUpDelay);
+});
+
+aboutMeButton.addEventListener("click", () => {
+	const about = document.querySelector('#aboutMe').getBoundingClientRect()
+	console.log(about)
+})
