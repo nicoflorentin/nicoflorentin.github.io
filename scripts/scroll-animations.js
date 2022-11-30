8804721c4cb3a60a747e3e3e3a8b6ade81273d74
Nicolás Florentín <nicogrunger@hotmail.com>
2022-11-30 00:08:13 -0300

    animations fix

--- scripts/scroll-animations.js
+++ scripts/scroll-animations.js
@@ -1,12 +1,19 @@
 const aboutMeSection = document.querySelector('#aboutMe');
 const skillsSection = document.querySelector('.skillsSection');
-const fadeIn_left = document.querySelector('.fadeIn_left')
-const fadeIn_right = document.querySelector('.fadeIn_right')
+const aboutMe_left = document.querySelector('.aboutMe__img')
+const aboutMe_right = document.querySelector('.aboutMeContainer')
+const skills_left = document.querySelector('.skillsContainer')
+const skills_right = document.querySelector('.skills__img')
 
-fadeIn_left.style.transform = 'translatex(-100px) translatey(100px)';
-fadeIn_left.style.opacity = '0';
-fadeIn_right.style.transform = 'translatex(100px) translatey(-100px)';
-fadeIn_right.style.opacity = '0';
+aboutMe_left.style.transform = 'translatex(-100px) translatey(100px)';
+aboutMe_left.style.opacity = '0';
+aboutMe_right.style.transform = 'translatex(100px) translatey(-100px)';
+aboutMe_right.style.opacity = '0';
+
+skills_left.style.transform = 'translatex(-100px) translatey(100px)';
+skills_left.style.opacity = '0';
+skills_right.style.transform = 'translatex(100px) translatey(-100px)';
+skills_right.style.opacity = '0';
 
 const diagonalAnimate = (el_left, el_right) => {
 
@@ -24,16 +31,14 @@ const diagonalAnimate = (el_left, el_right) => {
 // Intersection Observer API
 var aboutme_observer = new IntersectionObserver(function(entries) {
     if(entries[0].isIntersecting === true) {
-        diagonalAnimate(fadeIn_left, fadeIn_right);
+        diagonalAnimate(aboutMe_left, aboutMe_right);
     } 
 }, { threshold: [0.7] });
 aboutme_observer.observe(aboutMeSection);
 
 var skills_observer = new IntersectionObserver(function(entries) {
     if(entries[0].isIntersecting === true) {
-        diagonalAnimate(fadeIn_left, fadeIn_right);
+        diagonalAnimate(skills_left, skills_right);
     } 
 }, { threshold: [0.7] });
 skills_observer.observe(skillsSection);
-
-
