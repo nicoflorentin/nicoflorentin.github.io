bc56826b340bb351be639ca3fcdadd8e1316defa
Nicolás Florentín <nicogrunger@hotmail.com>
2022-12-29 18:55:11 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
-import { defineConfig } from 'vite'
-
-export default defineConfig(
-    {
-        base: '/'
-    }
-)
+// vite.config.js
+export default {
+    // config options
+    base: '/'
+  }
